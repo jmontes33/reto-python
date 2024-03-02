ecedb80eb551986084e02e67329b57dacd92ae01
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-03-02 21:28:53 +0100

    practice_2 improvement

--- practice_2.py
+++ practice_2.py
@@ -3,11 +3,5 @@ import pandas as pd
 df = pd.read_csv('reto.csv')
 
 for _, row in df.iterrows():
-    data = {
-        "Matricula": row["Matricula"],
-        "Latitud": row["Latitud"],
-        "Longitud": row["Longitud"],
-        "Distance": row["Distance"],
-        "Pos_date": row["Pos_date"]
-    }
+    data = row.to_json()
     print(data)
