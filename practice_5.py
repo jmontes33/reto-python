f8070dbcb0f12f1f149df04b1eb87d7208999894
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-03-03 15:15:36 +0100

    practice_5 improvements

--- practice_5.py
+++ practice_5.py
@@ -10,8 +10,10 @@ df['Pos_date'] = pd.to_datetime(df['Pos_date'], unit='ms', errors='coerce')
 
 df = df.sort_values(by='Pos_date', ascending=False)
 
-df['Pos_date'] = df['Pos_date'].apply(lambda x: x.strftime('%d/%m/%Y %H:%M:%S'))
+df_last_positions = df.groupby('Matricula').first().reset_index()
 
-df.to_csv('ultima_posicion_vehiculos.txt', index=False, columns=['Matricula', 'Pos_date'])
+df_last_positions['Pos_date'] = df_last_positions['Pos_date'].dt.strftime('%d/%m/%Y %H:%M:%S')
+
+df_last_positions.to_csv('ultima_posicion_vehiculos.txt', index=False, columns=['Matricula', 'Pos_date'])
 
 print("Archivo 'ultima_posicion_vehiculos.txt' creado")
